8e5ca1e3c1f6892627f278f49b7802adc0c3e0d4
ShaDi777 <dima.shalanov@mail.ru>
2023-10-03 20:40:26 +0300

    Init task 1 with tests

---
+++ src/main/java/edu/hw1/Task1.java
@@ -0,0 +1,28 @@
+package edu.hw1;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import java.util.Objects;
+
+public final class Task1 {
+    private final static Logger LOGGER = LogManager.getLogger();
+
+    private Task1() {
+    }
+
+    public static long GetVideoLengthSeconds(String time) {
+        Objects.requireNonNull(time);
+        LOGGER.trace("Decoding time in seconds from " + time);
+        LOGGER.trace("Time matches regex? " + time.matches("\\d\\d+:\\d\\d"));
+
+        if (!time.matches("\\d\\d+:\\d\\d")) return -1;
+
+        int minutes = Integer.parseInt(time.split(":", 2)[0]);
+        int seconds = Integer.parseInt(time.split(":", 2)[1]);
+        LOGGER.trace("Get {} minutes and {} seconds ", minutes, seconds);
+
+        if (seconds >= 60) return -1;
+
+        return (minutes * 60L) + seconds;
+    }
+}
---
+++ src/test/java/edu/hw1/Task1Test.java
@@ -0,0 +1,54 @@
+package edu.hw1;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+import org.junit.jupiter.params.provider.ValueSource;
+import java.util.stream.Stream;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+public class Task1Test {
+
+    @ParameterizedTest
+    @ValueSource(
+        strings = {
+            ":",
+            "0:0",
+            "12:",
+            ":12",
+            "1:10",
+            "10:0",
+            "10:60",
+            "11:99"
+        }
+    )
+    void getTimeFromIncorrectString(String time) {
+        // Arrange in parameters
+        // Act
+        long timeInSeconds = Task1.GetVideoLengthSeconds(time);
+
+        // Assert
+        assertThat(timeInSeconds).isEqualTo(-1);
+    }
+
+
+    private static Stream<Arguments> paramsCorrectStringWithResults() {
+        return Stream.of(
+            Arguments.of("00:00", 0),
+            Arguments.of("00:59", 59),
+            Arguments.of("01:00", 60),
+            Arguments.of("13:56", 836),
+            Arguments.of("999:59", 59999)
+        );
+    }
+    @ParameterizedTest
+    @MethodSource("paramsCorrectStringWithResults")
+    void getTimeFromCorrectString(String time, long expectedResult) {
+        // Arrange in parameters
+        // Act
+        long timeInSeconds = Task1.GetVideoLengthSeconds(time);
+
+        // Assert
+        assertThat(timeInSeconds).isEqualTo(expectedResult);
+    }
+}
