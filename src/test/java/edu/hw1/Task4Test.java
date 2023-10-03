74e331d3d0d579d2a72b90795bd6382fd3ba7ae7
ShaDi777 <dima.shalanov@mail.ru>
2023-10-03 22:16:29 +0300

    Init task 4 with tests

---
+++ src/test/java/edu/hw1/Task4Test.java
@@ -0,0 +1,31 @@
+package edu.hw1;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+import java.util.stream.Stream;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+
+public class Task4Test {
+    private static Stream<Arguments> paramsBrokenAndFixedStrings() {
+        return Stream.of(
+            Arguments.of("abc", "bac"),
+            Arguments.of("123456", "214365"),
+            Arguments.of("badce", "abcde"),
+            Arguments.of("hTsii  s aimex dpus rtni.g", "This is a mixed up string."),
+            Arguments.of("", ""),
+            Arguments.of("Русские буквы.", "уРссик еубвк.ы")
+        );
+    }
+    @ParameterizedTest
+    @MethodSource("paramsBrokenAndFixedStrings")
+    void checkNotNestedArrays(String broken, String expectedFix) {
+        // Arrange in parameters
+        // Act
+        String result = Task4.fixString(broken);
+
+        // Assert
+        assertThat(result).isEqualTo(expectedFix);
+    }
+}
